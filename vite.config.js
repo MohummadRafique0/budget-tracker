007f8c9b558e21436bb3f0f7bea5fcea0d6f5561
MohummadRafique0 <yomoplaz@gmail.com>
2024-09-14 10:34:35 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/MohummadRafique0/budget-tracker",
+  base: "/budget-tracker/",
   plugins: [react()],
   build: {
     outDir: "dist", // Default Vite output directory
